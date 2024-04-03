78c30cd99acae418e4ee17e6b9842afd3d11382d
johnpetros <joaopedro.nc@outlook.com>
2024-04-03 15:32:20 -0300

    🖥️ views: setup sensors and checklist views resolve #8

--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from views import init_views
 
+# flask --app ./src/app/main.py run --debug -p 5000
 
-def init_app():
+def create_app():
     app = Flask(
-        __name__, template_folder="../ui/templates", static_folder="../ui/static"
+        __name__,
+        template_folder="../ui/templates", 
+        static_folder="../ui/static"
     )
-
+    init_views(app)
+    
     return app
