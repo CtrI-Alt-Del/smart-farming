4628eb3bd6e97859cccf7370e002dcddfd476158
0thigs <machinimavlogs@gmail.com>
2024-04-03 08:50:00 -0300

    Thigs First Commit

--- src/app/main.py
+++ src/app/main.py
@@ -3,7 +3,9 @@ from flask import Flask
 
 def init_app():
     app = Flask(
-        __name__, template_folder="../ui/templates", static_folder="../ui/static"
+        __name__,
+        template_folder="../ui/templates", 
+        static_folder="../ui/static"
     )
-
+    
     return app
