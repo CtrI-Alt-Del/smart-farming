16af07240b9facd183c831d3ff085b51362864e5
johnpetros <joaopedro.nc@outlook.com>
2024-04-06 18:02:39 -0300

    ⚙️ config: set app secret key

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,7 @@
+from os import getenv
+
 from flask import Flask
+
 from views import init_views
 
 
@@ -6,6 +9,9 @@ def init_app():
     app = Flask(
         __name__, template_folder="../ui/templates", static_folder="../ui/static"
     )
+
+    app.config["SECRET_KEY"] = getenv("SECRET_KEY")
+
     init_views(app)
 
     return app
