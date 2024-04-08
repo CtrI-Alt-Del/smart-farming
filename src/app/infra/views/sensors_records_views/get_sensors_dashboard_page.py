fea97c6895cbab4d1f1081a3b366e4b3fa430e43
0thigs <machinimavlogs@gmail.com>
2024-04-08 20:24:59 -0300

    🖥views: sensors dashboard page view

--- src/app/infra/views/sensors_records_views/get_sensors_dashboard_page.py
+++ src/app/infra/views/sensors_records_views/get_sensors_dashboard_page.py
@@ -5,4 +5,4 @@ from core.use_cases.sensors_records import get_sensors_dashboard_page_data
 
 def get_sensors_dashboard_page_view():
     result = get_sensors_dashboard_page_data.execute()
-    return render_template("/pages/sensors_dashboard.html", sensors_record_data=result)
+    return render_template("/pages/sensors_records_dashboard/index.html", sensors_record_data=result)
