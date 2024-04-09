fcabd90777892dfe37c9484c029ac2ce272389a1
FaelSantoss <silva.faelm@gmail.com>
2024-04-09 10:13:04 -0300

    💡 scripts: chart filter

--- src/app/core/commons/chart_filter.py
+++ src/app/core/commons/chart_filter.py
@@ -1,4 +1,5 @@
 from datetime import date,timedelta
+from infra.utils.error import Error
 
 class ChartFilter:
 
@@ -9,9 +10,12 @@ class ChartFilter:
     def filter_records_by_range_of_date(self , range_days):
         current_date = date.today()
         wanted_data = []
-        for day in range(range_days,0,-1):
-            wanted_date = current_date - timedelta(days=day)
-            for record in self.records:
-                if record.created_at == wanted_date:
-                    wanted_data.append(record)
+        try:
+            for day in range(range_days,0,-1):
+                wanted_date = current_date - timedelta(days=day)
+                for record in self.records:
+                    if record.created_at.date() == wanted_date:
+                        wanted_data.append(record)
+        except Error as error:
+            raise error
         return wanted_data
--- src/app/core/use_cases/sensors_records/get_sensors_dashboard_page_data.py
+++ src/app/core/use_cases/sensors_records/get_sensors_dashboard_page_data.py
@@ -1,33 +1,13 @@
-from datetime import date, timedelta
 from infra.repositories import sensors_records_repository
-
+from core.commons.chart_filter import ChartFilter
 
 class GetSensorDashboardPageData:
     def execute(self):
         sensor_records = sensors_records_repository.get_sensor_records()
+        chartFilter = ChartFilter(sensor_records)
         options = {
-            "150 days": self.filter_sensor_records_by_range_of_date(
-                150, sensor_records
-            ),
-            "300 days": self.filter_sensor_records_by_range_of_date(
-                300, sensor_records
-            ),
-            "450 days": self.filter_sensor_records_by_range_of_date(
-                450, sensor_records
-            ),
+            "150 days": chartFilter.filter_records_by_range_of_date(150),
+            "300 days": chartFilter.filter_records_by_range_of_date(300),
+            "450 days": chartFilter.filter_records_by_range_of_date(450),
         }
-        print(options)
         return options
-
-    def filter_sensor_records_by_range_of_date(self, range_days, sensor_records):
-        current_date = date.today()
-        wanted_data = []
-        for day in range(range_days, 0, -1):
-            wanted_date = current_date - timedelta(days=day)
-            for sensors_record in sensor_records:
-                created_at_date = sensors_record.created_at.date()
-
-                if created_at_date == wanted_date:
-                    wanted_data.append(sensors_record)
-
-        return wanted_data
