061b1d5e14166aa049789885ea6defffb1eefceb
0thigs <machinimavlogs@gmail.com>
2024-04-09 00:42:51 -0300

    ⚙️config: tailwind and package json docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/ui/templates/**/*.html",
-    "./src/ui/static/src/**/*.js",
-  ],
+  content: ['./src/ui/templates/**/*.html', './src/ui/static/src/**/*.js'],
   theme: {
     extend: {
       colors: {
         green: {
-          default: "#2CA24D",
+          default: '#2CA24D',
         },
         yellow: {
-          default: "#F5B10F",
-          dark: "#C7A700"
-        }
-      }
+          default: '#da9e13',
+          dark: '#C7A700',
+        },
+      },
     },
   },
   plugins: [
     require('flowbite/plugin')({
       charts: true,
-  })
+    }),
   ],
 }
