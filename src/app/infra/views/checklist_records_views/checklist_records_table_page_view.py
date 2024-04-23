9391cff5d245b63d6fcd7bb28ff9ea7620aa49f3
JohnPetros <joaopedro.nc@outlook.com>
2024-04-22 23:48:44 -0300

    🖥️ views: pass checklist_record_form to checklist_records_table template

--- src/app/infra/views/checklist_records_views/checklist_records_table_page_view.py
+++ src/app/infra/views/checklist_records_views/checklist_records_table_page_view.py
@@ -1,5 +1,12 @@
 from flask import render_template
 
+from infra.forms.checklist_record_form import ChecklistRecordForm
+
 
 def checklist_records_table_page_view():
-    return render_template("/pages/checklist_records_table/index.html")
+    checklist_record_form = ChecklistRecordForm()
+
+    return render_template(
+        "/pages/checklist_records_table/index.html",
+        checklist_record_form=checklist_record_form,
+    )
