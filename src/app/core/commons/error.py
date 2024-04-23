786efd8f269515352ed3a46f51572a7db1840ae0
JohnPetros <joaopedro.nc@outlook.com>
2024-04-22 23:43:15 -0300

    🪙 commons: add status_code attribute to Error

--- src/app/core/commons/error.py
+++ src/app/core/commons/error.py
@@ -6,8 +6,10 @@ class Error(Exception):
         self,
         ui_message="Pultz, algo deu errado",
         internal_message="Internal Server Error",
+        status_code=500,
     ):
         self.ui_message = ui_message
         self.internal_message = internal_message
+        self.status_code = status_code
 
         cow_say(internal_message)
