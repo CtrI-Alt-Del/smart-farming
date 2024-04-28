6187c4da523a1e4a681270a7ee97899aec59760b
JohnPetros <joaopedro.nc@outlook.com>
2024-04-28 19:42:51 -0300

    🗃️ file: rename plants repository

--- src/app/infra/repositories/__init__.py
+++ src/app/infra/repositories/__init__.py
@@ -1,7 +1,7 @@
 from .checklist_records_repository import CheckListRecordsRepository
 from .sensors_records_repository import SensorRecordsRepository
-from .plant_repository import PlantRepository
+from .plants_repository import PlantsRepository
 
 checklist_records_repository = CheckListRecordsRepository()
 sensors_records_repository = SensorRecordsRepository()
-plant_repository = PlantRepository()
+plants_repository = PlantsRepository()
