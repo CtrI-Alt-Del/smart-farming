2ab32ceac6ed13fcc9068141d4791ec2ec4fe195
JohnPetros <joaopedro.nc@outlook.com>
2024-04-28 22:14:30 -0300

    ♻️ refactor: remove create plant vy csv file

--- src/app/infra/views/plants_views/__init__.py
+++ src/app/infra/views/plants_views/__init__.py
@@ -6,4 +6,4 @@ plants_views = Blueprint("plants_views", __name__)
 
 route = plants_views.add_url_rule
 
-route(rule="/plants", view_func=plants_page_view, methods=["GET"])
+route("/plants", view_func=plants_page_view, methods=["GET"])
--- src/app/infra/views/plants_views/plants_page_view.py
+++ src/app/infra/views/plants_views/plants_page_view.py
@@ -1,6 +1,18 @@
 from flask import render_template
 
+from infra.repositories import plants_repository
+from infra.forms import PlantForm
+
 
 def plants_page_view():
+    plants = plants_repository.get_plants()
 
-    return render_template("pages/plants/index.html")
+    create_plant_record_form = PlantForm()
+    update_plant_record_form = PlantForm()
+
+    return render_template(
+        "pages/plants/index.html",
+        create_plant_record_form=create_plant_record_form,
+        update_plant_record_form=update_plant_record_form,
+        plants=plants,
+    )
