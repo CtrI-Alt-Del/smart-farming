4ede2195e6c75ca181342559fcc185e5a03afe6a
0thigs <machinimavlogs@gmail.com>
2024-04-30 00:21:33 -0300

    💡scripts: handle sensors records table pagination

--- src/app/core/use_cases/sensors_records/get_sensors_records_table_page_data.py
+++ src/app/core/use_cases/sensors_records/get_sensors_records_table_page_data.py
@@ -1,16 +1,24 @@
-from typing import Tuple
+from math import ceil
 
 from core.entities import Plant, SensorsRecord
+from core.constants import PAGINATION_LIMIT
 
 from infra.repositories import plants_repository
-
 from infra.repositories import sensors_records_repository
 
 class GetSensorsRecordsTablePageData:
-    def execute(self,page_number: int) -> Tuple[list[Plant] , list[SensorsRecord]]:
-        plants = plants_repository.get_plants()
-        
+    def execute(self, page_number: int = 1, should_get_plants: bool = False) -> tuple[list[SensorsRecord], int, list[Plant]]:
+
+        plants = []
+        if should_get_plants:
+            plants = plants_repository.get_plants()
+
         sensors_records = sensors_records_repository.get_filtered_sensors_records(
-            page_number=page_number
+            page_number=int(page_number)
         )
-        return plants,sensors_records
+
+        sensors_records_count = sensors_records_repository.get_sensors_records_count()
+
+        pages_count = ceil(sensors_records_count / PAGINATION_LIMIT)
+
+        return sensors_records, pages_count, plants
