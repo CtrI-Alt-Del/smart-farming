412badff844d74b7630195a42c953cfa479995c8
0thigs <machinimavlogs@gmail.com>
2024-04-30 19:09:21 -0300

    🧱templates: card plant template again

--- src/app/infra/database/scripts/plants.sql
+++ src/app/infra/database/scripts/plants.sql
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
--- Active: 1714396197562@@127.0.0.1@3306@smart-farming
-=======
--- Active: 1714479268496@@127.0.0.1@3306@smart-farming
->>>>>>> refs/remotes/origin/main
+
 DROP TABLE IF EXISTS plants;
 
 CREATE TABLE IF NOT EXISTS plants (
