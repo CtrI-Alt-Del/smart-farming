4dffe0fe246edd6d248414aeabef111ccce3c41c
JohnPetros <joaopedro.nc@outlook.com>
2024-05-01 15:35:20 -0300

    🖥️ views: update_checklist_record_form_view

---
+++ src/app/infra/views/checklist_records_views/update_checklist_record_form_view.py
@@ -0,0 +1,23 @@
+from flask import render_template
+
+from core.commons import Error
+
+from infra.forms import ChecklistRecordForm
+from infra.repositories import checklist_records_repository
+
+
+def update_checklist_record_form_view(id: str):
+    try:
+        checklist_record = checklist_records_repository.get_checklist_record_by_id(id)
+
+        update_checklist_record_form = ChecklistRecordForm(
+            checklist_record=checklist_record
+        )
+
+        return render_template(
+            "pages/checklist_records_table/update_checklist_record_form/index.html",
+            checklist_record_id=checklist_record.id,
+            update_checklist_record_form=update_checklist_record_form,
+        )
+    except Error as error:
+        return "Registro não encontrado 😢", error.status_code
