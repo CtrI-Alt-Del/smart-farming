b05134c7ffb8d0604a8e183f83e830d647f6761d
JohnPetros <joaopedro.nc@outlook.com>
2024-05-01 15:41:06 -0300

    🪨 constants: update checklist table

--- src/app/infra/database/scripts/checklist_records.sql
+++ src/app/infra/database/scripts/checklist_records.sql
@@ -1,4 +1,4 @@
--- Active: 1712147539997@@127.0.0.1@3306@smart-farming
+-- Active: 1713899386704@@127.0.0.1@3306@smart-farming
 DROP TABLE IF EXISTS checklist_records;
 
 INSERT INTO checklist_records (
@@ -28,18 +28,8 @@ INSERT INTO checklist_records (
   'PLANTIO INTERNO(FATEC)', -- plantation_type
   CURDATE(), -- fertilizer_expiration_date
   'Relatório de checagem', -- report
-  '4544afe3-0661-11ef-9512-0242ac140002' -- plant_id
+  '1ded0f79-01a5-11ef-9b63-0242ac1b0002' -- plant_id
 );
-
-SELECT CR.*, P.id AS plant_id, P.name AS plant_name, P.id AS plant_id, P.hex_color AS plant_color
-FROM checklist_records AS CR
-JOIN plants AS P.id = CR.plant_id
-WHERE CR.id = '';
-
-SELECT CR.*, P.id AS plant_id, P.name AS plant_name, P.hex_color AS plant_color
-FROM checklist_records AS CR 
-JOIN plants AS P ON P.id = CR.plant_id
-ORDER BY created_at DESC;
 
 INSERT INTO checklist_records (
   soil_ph,
@@ -68,7 +58,12 @@ INSERT INTO checklist_records (
   'PLANTIO INTERNO (FATEC)', -- plantation_type
   '2024-03-03', -- fertilizer_expiration_date
   'Relatório de checagem', -- report
-  '4544afe3-0661-11ef-9512-0242ac140002' -- plant_id
+  '1ded0f79-01a5-11ef-9b63-0242ac1b0002' -- plant_id
 );
+
+SELECT *, P.id AS plant_id, P.name AS plant_name
+FROM checklist_records AS CR 
+JOIN plants AS P ON P.id = CR.plant_id
+ORDER BY created_at DESC;
 
 DELETE FROM checklist_records;
