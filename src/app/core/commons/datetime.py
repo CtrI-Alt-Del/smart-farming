3043950ad12f9e4bdb75ee1fe338247313f7f5aa
JohnPetros <joaopedro.nc@outlook.com>
2024-05-01 15:35:00 -0300

    🪙 commons: get value as date or datetime

--- src/app/core/commons/datetime.py
+++ src/app/core/commons/datetime.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 
 class Datetime:
+    value: datetime
+
     def __init__(self, value: datetime):
         self.value = value
 
@@ -9,5 +11,11 @@ class Datetime:
         self.value = self.value.strftime("%d/%m/%Y %H:%M:%S")
         return self
 
-    def get_value(self) -> str:
+    def get_hour(self) -> int:
+        return self.value.hour
+
+    def get_value(self, is_datetime: bool = False) -> str:
+        if is_datetime:
+            return self.value
+
         return str(self.value)
