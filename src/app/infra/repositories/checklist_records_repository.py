ba22420c4c4e2a9de30fec7b3d928784b186f6ce
JohnPetros <joaopedro.nc@outlook.com>
2024-05-01 15:36:13 -0300

    🗄️ repositories: get_checklist_record_by_id

--- src/app/infra/repositories/checklist_records_repository.py
+++ src/app/infra/repositories/checklist_records_repository.py
@@ -5,7 +5,7 @@ from core.constants import PAGINATION
 from infra.database import mysql
 
 
-class CheckListRecordsRepository:
+class ChecklistRecordsRepository:
     def create_checklist_record(self, checklist_record: CheckListRecord) -> None:
         sql = """
         INSERT INTO checklist_records
@@ -98,25 +98,13 @@ class CheckListRecordsRepository:
             ],
         )
 
-    def get_checklist_record_by_id(self, id: str) -> CheckListRecord | None:
-        row = mysql.query(
-            sql="SELECT * FROM checklist_records WHERE id = %s",
-            is_single=True,
-            params=[id],
-        )
-
-        if row:
-            return self.__get_checklist_record_entity(row)
-
-        return None
-
     def get_filtered_checklist_records(self, page_number: int) -> list[CheckListRecord]:
         pagination_limit = PAGINATION["records_per_page"]
         offset = (page_number - 1) * pagination_limit
 
         rows = mysql.query(
             sql=f"""
-            SELECT *, P.id AS plant_id, P.name AS plant_name, P.hex_color AS plant_color
+            SELECT CR.*, P.id AS plant_id, P.name AS plant_name, P.hex_color AS plant_color
             FROM checklist_records AS CR 
             JOIN plants AS P ON P.id = CR.plant_id
             ORDER BY created_at DESC
@@ -132,13 +120,30 @@ class CheckListRecordsRepository:
 
         return checklist_records
 
-    def get_checklist_records_count(self):
+    def get_checklist_records_count(self) -> int:
         result = mysql.query(
             sql="SELECT COUNT(id) AS count FROM checklist_records",
             is_single=True,
         )
 
         return result["count"]
+
+    def get_checklist_record_by_id(self, id: str) -> CheckListRecord | None:
+        row = mysql.query(
+            sql="""
+            SELECT CR.*, P.id AS plant_id, P.name AS plant_name, P.hex_color AS plant_color
+            FROM checklist_records AS CR 
+            JOIN plants AS P ON P.id = CR.plant_id
+            WHERE CR.id = %s
+            """,
+            is_single=True,
+            params=[id],
+        )
+
+        if not row:
+            return None
+
+        return self.__get_checklist_record_entity(row)
 
     def __get_checklist_record_entity(self, row: dict):
         return CheckListRecord(
