210f6d91c0645ded6d8f5260627e385528ed7d31
JohnPetros <joaopedro.nc@outlook.com>
2024-05-03 15:16:53 -0300

    🖥️ views: parse plant_growth_chart_data to json

--- src/app/infra/views/checklist_records_views/checklist_records_dashboard_page_view.py
+++ src/app/infra/views/checklist_records_views/checklist_records_dashboard_page_view.py
@@ -5,19 +5,25 @@ from core.use_cases.checklist_records import get_checklist_dashboard_page_data
 from core.constants import LEAF_COLORS
 
 from infra.constants import LEAF_COLORS_CHART_LEGEND_HEX_COLORS
+from infra.utils import JSONEncoder
 
 
 def checklist_records_dashboard_page_view():
     data = get_checklist_dashboard_page_data.execute()
+
     leaf_appearences_chart_data = dumps(
         data["days_count_by_leaf_appearance_and_plant"], ensure_ascii=False
     )
+
     leaf_colors_chart_data = dumps(
         data["days_count_by_leaf_color_and_plant"], ensure_ascii=False
     )
+
+    plant_growth_chart_data = dumps(data["plant_growth_chart_data"], cls=JSONEncoder)
+
     plants = data["plants"]
 
-    print(dumps(leaf_colors_chart_data), flush=True)
+    # print(dumps(data["plant_growth_chart_data"]), flush=True)
 
     return render_template(
         "pages/checklist_records_dashboard/index.html",
@@ -25,5 +31,6 @@ def checklist_records_dashboard_page_view():
         leaf_colors_chart_data=leaf_colors_chart_data,
         leaf_colors_chart_hex_colors=LEAF_COLORS_CHART_LEGEND_HEX_COLORS,
         leaf_colors=LEAF_COLORS,
+        plant_growth_chart_data=plant_growth_chart_data,
         plants=plants,
     )
