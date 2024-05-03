cd5a02239d08dbb68bf7e00ff34fd2a9b7ba152e
JohnPetros <joaopedro.nc@outlook.com>
2024-05-03 15:17:21 -0300

    🪙 commons: validate date key in chart records

--- src/app/core/commons/chart.py
+++ src/app/core/commons/chart.py
@@ -1,18 +1,24 @@
 from typing import List, Dict
 from datetime import timedelta
 
+from core.commons.error import Error
+
 
 class Chart:
     def __init__(self, records: List[Dict]) -> None:
+        for record in records:
+            if "date" not in record:
+                raise Error("Date attribute not found in record")
+
         self.records = records
 
     def filter_records_by_range_of_days(self, days_range):
-        last_date = self.records[-1]["date"]
+        last_record = self.records[-1]
+        last_date = last_record["date"]
         data = []
 
         for day in range(days_range, -1, -1):
             current_date = last_date - timedelta(days=day)
-            # print(f"days_range: {days_range}", current_date, flush=True)
 
             for record in self.records:
                 if record["date"] == current_date:
