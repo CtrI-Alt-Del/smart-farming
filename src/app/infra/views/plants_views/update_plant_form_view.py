17ccc6fb7dcb16b36092d5e0ea9ce59317bd10a5
JohnPetros <joaopedro.nc@outlook.com>
2024-05-03 22:18:19 -0300

    🖥️ views: return form template to update a plant

--- src/app/infra/views/plants_views/update_plant_form_view.py
+++ src/app/infra/views/plants_views/update_plant_form_view.py
@@ -1,4 +1,4 @@
-from flask import request, render_template
+from flask import render_template
 
 from core.use_cases.plants_records import get_plant_by_id
 from core.commons import Error
@@ -8,11 +8,12 @@ from infra.forms import PlantForm
 
 def update_plant_form_view(id: str):
     try:
-        plant = get_plant_by_id.execute()
+        plant = get_plant_by_id.execute(id)
+        plant_form = PlantForm(plant=plant)
 
         return render_template(
-            "pages/plants/create_plant_form/index.html",
-            plant=plant,
+            "pages/plants/update_plant_form/index.html",
+            update_plant_form=plant_form,
         )
     except Error as error:
         print(plant_form.errors, flush=True)
