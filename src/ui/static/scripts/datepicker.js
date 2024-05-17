99c41ab47571d69bcd63a30b463ca47e6fd4ae7c
Tico1606 <gabrielsoliveira1606@gmail.com>
2024-05-17 13:05:11 -0300

    🐛 fix: synchronize dates from datepicker component

--- src/ui/static/scripts/datepicker.js
+++ src/ui/static/scripts/datepicker.js
@@ -1,36 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const startDateInput = document.getElementById("startDate")
-  const endDateInput = document.getElementById("endDate")
-  let previousEndDateValue = endDateInput.value
+  const startDateInput = document.getElementById("start-date");
+  const endDateInput = document.getElementById("end-date");
+  let previousEndDateValue = endDateInput.value;
 
-  endDateInput.disabled = true
-  endDateInput.style.opacity = "0.5"
-  endDateInput.style.cursor = "not-allowed"
+  endDateInput.style.opacity = "0.5";
+  endDateInput.style.cursor = "not-allowed";
+
+  endDateInput.disabled = true; 
 
   function setEndDateToStartDate() {
     if (!endDateInput.value) {
-      endDateInput.value = startDateInput.value
+      endDateInput.value = startDateInput.value;
     }
   }
 
   startDateInput.addEventListener("blur", () => {
-    setEndDateToStartDate()
-  })
+    setEndDateToStartDate();
+  });
 
   endDateInput.addEventListener("input", () => {
-    if (new Date(startDateInput.value) > new Date(this.value)) {
-      this.value = previousEndDateValue
+    if (new Date(startDateInput.value) > new Date(endDateInput.value)) {
+      endDateInput.value = previousEndDateValue;
     } else {
-      previousEndDateValue = this.value
+      previousEndDateValue = endDateInput.value;
     }
-  })
+  });
 
-  startDateInput.addEventListener("change", () => {
-    const selectedDate = new Date(this.value)
-    selectedDate.setDate(selectedDate.getDate() + 1)
-    endDateInput.min = selectedDate.toISOString().split("T")[0]
-    endDateInput.disabled = false
-    endDateInput.style.opacity = "1"
-    endDateInput.style.cursor = "auto"
-  })
-})
+  startDateInput.addEventListener("change", (event) => {
+    const selectedDate = new Date(event.target.value);
+    if (selectedDate > new Date()) {
+      event.target.value = ""; 
+      alert("Datas futuras não são permitidas."); 
+      return; 
+    }
+    selectedDate.setDate(selectedDate.getDate() + 1); 
+    endDateInput.min = selectedDate.toISOString().split("T")[0];
+    endDateInput.disabled = false; 
+    endDateInput.style.opacity = "1"; 
+    endDateInput.style.cursor = "auto"; 
+  });
+});
