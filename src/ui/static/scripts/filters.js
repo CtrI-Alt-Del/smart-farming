2be59d475aecaeae4bd863624f1d53226ded9725
JohnPetros <joaopedro.nc@outlook.com>
2024-05-25 11:32:51 -0300

    💡 scripts: reset all filters on click on remove all button

--- src/ui/static/scripts/filters.js
+++ src/ui/static/scripts/filters.js
@@ -1,21 +1,64 @@
 class Filters {
   constructor() {
     const filters = document.querySelectorAll("[data-filters='filter']")
+    const removeAllButton = document.querySelector("[data-filters='remove-all-button']")
 
     if (!filters.length) return
 
+    this.filters = filters
     this.queryParam = new QueryParam()
 
-    for (const filter of filters) {
+    for (const filter of this.filters) {
       filter.addEventListener("input", (event) =>
         this.handleFilterChange(event),
       )
     }
+
+    if (removeAllButton) {
+      this.removeAllButton = removeAllButton
+      this.removeAllButton.addEventListener("click", () => this.handleRemoveAllButtonClick())
+
+      const currentParams = this.queryParam.getAll()
+      if (currentParams.length) this.showRemoveAllButton()
+    }
+  }
+
+  resetFilter(filter) {
+    switch (filter.nodeName) {
+      case "SELECT":
+        filter.selectedIndex = 0
+        break
+      default:
+        filter.value = ""
+    }
+
+    this.queryParam.remove(filter.name)
+  }
+
+  showRemoveAllButton() {
+    this.removeAllButton.classList.remove("hidden")
+  }
+
+  hideRemoveAllButton() {
+    this.removeAllButton.classList.add("hidden")
+  }
+
+  handleRemoveAllButtonClick() {
+    for (const filter of this.filters) {
+      this.resetFilter(filter)
+    }
+
+    const changeEvent = new Event('change', { bubbles: true })
+    this.filters[0].dispatchEvent(changeEvent)
+    this.hideRemoveAllButton()
   }
 
   handleFilterChange(event) {
     const field = event.currentTarget
     this.queryParam.append(field.name, field.value)
+
+    if (this.removeAllButton)
+      this.showRemoveAllButton()
   }
 }
 
