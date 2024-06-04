5239ae2d65c863b06b5a009e5f7f195de4d3018b
JohnPetros <joaopedro.nc@outlook.com>
2024-06-04 09:58:23 -0300

    ♻️ refactor: ensure DataAnalyserProvider have inner dataframe atrribute

--- src/app/infra/providers/data_analyser_provider.py
+++ src/app/infra/providers/data_analyser_provider.py
@@ -1,37 +1,43 @@
-from typing import Dict, List
+from typing import List, Dict
 
 from werkzeug.datastructures import FileStorage
 
-from pandas import DataFrame, read_excel, read_csv
+from pandas import DataFrame, read_csv, read_excel
 
 
 class DataAnalyserProvider:
-    data: DataFrame | None
+    dataframe: DataFrame
 
     def __init__(self) -> None:
-        self.data = None
+        self.dataframe = None
 
     def analyse(self, data) -> None:
-        self.data = DataFrame(data)
+        self.dataframe = DataFrame(data)
 
     def read_excel(self, file: FileStorage) -> None:
-        self.data = read_excel(file)
+        self.dataframe = DataFrame(read_excel(file).dropna())
 
     def read_csv(self, file: FileStorage) -> None:
-        self.data = read_csv(file)
+        self.dataframe = DataFrame(read_csv(file).dropna())
+
+    def get_columns(self) -> List[str] | None:
+        if self.__has_dataframe():
+            return list(self.dataframe.columns)
+
+        return None
 
     def convert_to_excel(self, folder: str, filename: str) -> None:
         if self.__has_dataframe():
-            self.data.to_excel(f"{folder}/{filename}", index=False)
+            self.dataframe.to_excel(f"{folder}/{filename}", index=False)
 
     def convert_to_list_of_records(self) -> List[Dict] | None:
         if self.__has_dataframe():
-            return self.data.to_dict("records")
+            return self.dataframe.to_dict("records")
 
         return None
 
     def get_data(self):
-        return self.data
+        return self.dataframe
 
     def __has_dataframe(self) -> bool:
-        return self.data is not None
+        return self.dataframe is not None
