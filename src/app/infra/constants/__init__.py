9f0c79266986b8c7a93997883f27c7ca2a7c12eb
JohnPetros <joaopedro.nc@outlook.com>
2024-06-04 00:29:22 -0300

    🪨 constants: add folders

--- src/app/infra/constants/__init__.py
+++ src/app/infra/constants/__init__.py
@@ -1,2 +1,3 @@
+from .folders import *
 from .mysql import *
 from .leaf_colors_chart_legend_hex_colors import *
---
+++ src/app/infra/constants/folders.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+path = Path()
+
+FOLDERS = {
+    "tmp": path.absolute() / "src" / "app" / "infra" / "tmp",
+}
