b349502605eb231dcc0e34eb6f38f3635b9d1453
JohnPetros <joaopedro.nc@outlook.com>
2024-06-04 19:49:46 -0300

    🗃️ files: rename users repository

--- src/app/infra/repositories/__init__.py
+++ src/app/infra/repositories/__init__.py
@@ -1,9 +1,9 @@
 from .checklist_records_repository import ChecklistRecordsRepository
 from .sensors_records_repository import SensorRecordsRepository
 from .plants_repository import PlantsRepository
-from .user_repository import UserRepository
+from .users_repository import UsersRepository
 
 checklist_records_repository = ChecklistRecordsRepository()
 sensors_records_repository = SensorRecordsRepository()
 plants_repository = PlantsRepository()
-user_repository = UserRepository()
+users_repository = UsersRepository()
--- src/app/infra/repositories/users_repository.py
+++ src/app/infra/repositories/users_repository.py
@@ -1,29 +1,30 @@
 from core.entities import User
 from infra.database import mysql
 
-class UserRepository:
+
+class UsersRepository:
     def get_user_by_id(self, id: str) -> User | None:
         row = mysql.query(
-            sql= "SELECT * FROM user WHERE id = %s",
+            sql="SELECT * FROM user WHERE id = %s",
             params=[id],
             is_single=True,
         )
 
         if row:
             return self.__get_user_entity(row)
-        
+
         return None
-    
+
     def get_user_by_email(self, email: str) -> User | None:
         row = mysql.query(
-            sql= "SELECT * FROM user WHERE email = %s",
+            sql="SELECT * FROM user WHERE email = %s",
             params=[email],
             is_single=True,
         )
 
         if row:
             return self.__get_user_entity(row)
-        
+
         return None
 
     def __get_user_entity(self, row):
