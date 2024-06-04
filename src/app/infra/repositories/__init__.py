487f74447e8f0db1cc456f972ab25ae41fdcce03
FaelSantoss <silva.faelm@gmail.com>
2024-06-04 16:00:00 -0300

    ✨feat: created user repository

--- src/app/infra/repositories/__init__.py
+++ src/app/infra/repositories/__init__.py
@@ -1,7 +1,9 @@
 from .checklist_records_repository import ChecklistRecordsRepository
 from .sensors_records_repository import SensorRecordsRepository
 from .plants_repository import PlantsRepository
+from .user_repository import UserRepository
 
 checklist_records_repository = ChecklistRecordsRepository()
 sensors_records_repository = SensorRecordsRepository()
 plants_repository = PlantsRepository()
+user_repository = UserRepository()
