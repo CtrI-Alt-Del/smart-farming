ff5b8214ed436ad5161204abd879bff5ef35d518
JohnPetros <joaopedro.nc@outlook.com>
2024-06-05 17:30:59 -0300

    🖥️ views: pass next page param to login form

--- src/app/infra/views/authentication_views/login_page_view.py
+++ src/app/infra/views/authentication_views/login_page_view.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import request, render_template
 
 from infra.forms import LoginForm
 
@@ -6,4 +6,8 @@ from infra.forms import LoginForm
 def login_page_view():
     login_form = LoginForm()
 
-    return render_template("pages/login/index.html", login_form=login_form)
+    next_page_param = request.args.get("next")
+
+    return render_template(
+        "pages/login/index.html", login_form=login_form, next_page_param=next_page_param
+    )
