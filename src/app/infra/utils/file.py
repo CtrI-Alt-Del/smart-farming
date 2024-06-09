03cfb39181acadf315c770b1887f6114a44fb532
JohnPetros <joaopedro.nc@outlook.com>
2024-06-09 17:15:52 -0300

    🧰 utils: compress file

--- src/app/infra/utils/file.py
+++ src/app/infra/utils/file.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from gzip import open as open_compressed_file
 
 from core.commons import Error
 
@@ -24,3 +25,14 @@ class File:
             raise Error(
                 f"Failed to delete {self.path.absolute()} file. Error: {exception}"
             ) from exception
+
+    def compress(self):
+        file_path = self.path.absolute()
+        compress_file_path = str(file_path) + ".gz"
+
+        with open(file_path, "rb") as file:
+            with open_compressed_file(compress_file_path, "wb") as compressed_file:
+                compressed_file.write(file.read())
+
+        self.delete()
+        self.path = Path(compress_file_path)
