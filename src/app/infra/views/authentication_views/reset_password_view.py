da28b7fcbcc26fb2c751046e3ae13e9b3eb0a384
joelberto <kauanfvale@gmail.com>
2024-06-14 22:09:29 -0300

    🖥️views: add reset password view

--- src/app/infra/views/authentication_views/reset_password_view.py
+++ src/app/infra/views/authentication_views/reset_password_view.py
@@ -1,8 +1,29 @@
-from flask import render_template,request
+from flask import render_template, request, make_response, redirect
 
+
+from core.commons import Error
+
+from infra.authentication import auth
 
 
 def reset_password_view():
-    print(request.cookies.get("Petros"), flush=True)
-
-    return render_template("pages/reset_password/index.html")
+    try:
+        email_token = request.args.get("token")
+        cookie = request.cookies.get("Petros")
+        if not cookie:
+            raise Error(
+                internal_message="Client token has expired",
+                ui_message="Seu token Expirou!,reenvie novamente para o email!",
+                status_code=401,
+            )
+        if auth.check_hash(email_token, cookie):
+            response = make_response(render_template("pages/reset_password/index.html"))
+            return response
+        else:
+            return redirect("/login")
+    except Error:
+        raise Error(
+            internal_message="Client token has expired",
+            ui_message="Seu token Expirou!",
+            status_code=401,
+        )
