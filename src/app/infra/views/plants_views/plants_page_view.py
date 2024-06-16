ef84ac064bf112c2c2515e47a1e4e93c7e66f606
JohnPetros <joaopedro.nc@outlook.com>
2024-06-16 10:34:37 -0300

    🖥️ views: pass all plants from database into plants page

--- src/app/infra/views/plants_views/plants_page_view.py
+++ src/app/infra/views/plants_views/plants_page_view.py
@@ -4,6 +4,7 @@ from core.use_cases.plants import filter_plants
 
 from infra.forms import PlantForm
 from infra.authentication import auth
+from infra.repositories import plants_repository
 
 
 def plants_page_view():
@@ -11,6 +12,8 @@ def plants_page_view():
     auth_user = auth.get_user()
 
     plant_name = request.args.get("search", "")
+
+    all_plants = plants_repository.get_plants()
 
     plants = filter_plants.execute(plant_name)
 
@@ -22,5 +25,6 @@ def plants_page_view():
         create_plant_form=create_plant_form,
         update_plant_form=update_plant_form,
         plants=plants,
+        all_plants=all_plants,
         auth_user=auth_user,
     )
