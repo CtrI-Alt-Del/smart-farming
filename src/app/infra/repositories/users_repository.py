f49fe28cb05ec086dbe69d735b675e20fc918f65
JohnPetros <joaopedro.nc@outlook.com>
2024-06-16 10:33:29 -0300

    🗄️ repositories: handle acitve plant id collumn

--- src/app/infra/repositories/users_repository.py
+++ src/app/infra/repositories/users_repository.py
@@ -3,7 +3,9 @@ from infra.database import mysql
 
 
 class UsersRepository:
-    def get_user_by_id(self, id: str) -> User | None:
+    def get_user_by_id(
+        self, id: str, should_include_password: bool = False
+    ) -> User | None:
         row = mysql.query(
             sql="SELECT * FROM user WHERE id = %s",
             params=[id],
@@ -11,7 +13,12 @@ class UsersRepository:
         )
 
         if row:
-            return self.__get_user_entity(row)
+            user = self.__get_user_entity(row)
+
+            if not should_include_password:
+                del user.password
+
+            return user
 
         return None
 
@@ -27,8 +34,16 @@ class UsersRepository:
 
         return None
 
+    def get_user_active_plant_id(self, email: str) -> User | None:
+        row = mysql.query(
+            sql="SELECT active_plant_id FROM user WHERE email = %s",
+            params=[email],
+            is_single=True,
+        )
+
+        return row["active_plant_id"] if row else None
+
     def update_password(self, email, new_password: str):
-        print(new_password, flush=True)
         mysql.mutate(
             """
             UPDATE user SET password = %s WHERE email = %s
@@ -36,5 +51,18 @@ class UsersRepository:
             params=[new_password, email],
         )
 
+    def update_active_plant(self, id: str, plant_id: str):
+        mysql.mutate(
+            """
+            UPDATE user SET active_plant_id = %s WHERE id = %s
+            """,
+            params=[plant_id, id],
+        )
+
     def __get_user_entity(self, row):
-        return User(id=row["id"], email=row["email"], password=row["password"])
+        return User(
+            id=row["id"],
+            email=row["email"],
+            password=row["password"],
+            active_plant_id=row["active_plant_id"],
+        )
