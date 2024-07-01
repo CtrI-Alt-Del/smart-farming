d42222a68fb817e99d84cc504c07e599388a5912
JohnPetros <joaopedro.nc@outlook.com>
2024-07-01 17:39:44 -0300

    🚨 errors: add csv file validation

---
+++ src/app/core/errors/validation/__init__.py
@@ -0,0 +1,5 @@
+from .csv_columns_not_valid_error import *
+from .csv_file_not_valid_error import *
+from .datetime_not_valid_error import *
+from .date_not_valid_error import *
+from .record_not_valid_error import *
