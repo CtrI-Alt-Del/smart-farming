528d0f529407f884de433ea1627da0d66008f417
JohnPetros <joaopedro.nc@outlook.com>
2024-07-01 17:35:10 -0300

    🧪 tests: weekday common

--- src/app/core/commons/weekday.py
+++ src/app/core/commons/weekday.py
@@ -3,9 +3,14 @@ from calendar import day_name
 
 from core.constants import WEEKDAYS
 
+from core.errors.validation import DateNotValidError
+
 
 class Weekday:
     def __init__(self, value: date):
+        if not isinstance(value, date):
+            raise DateNotValidError()
+
         self.value = WEEKDAYS[day_name[value.weekday()].lower()]
 
     def get_value(self):
