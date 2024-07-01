745050d5306fbc28bf3cf1e189a693afe69beb1b
JohnPetros <joaopedro.nc@outlook.com>
2024-06-30 21:48:22 -0300

    🚨 errors: add user email not provided error

---
+++ src/app/core/errors/authentication/__init__.py
@@ -0,0 +1,4 @@
+from .user_email_not_provided_error import *
+from .email_template_not_provided_error import *
+from .sender_password_not_provided_error import *
+from .incorrect_admin_user_email_error import *
---
+++ src/app/core/errors/authentication/user_email_not_provided_error.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+from ..base_error import BaseError
+
+
+@dataclass
+class UserEmailNotProvidedError(BaseError):
+    ui_message = "E-mail de usuário não fornecido"
+    internal_message = "User email is not provided"
+    status_code = 500
