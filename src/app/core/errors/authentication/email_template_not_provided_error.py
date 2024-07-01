a8f3688df20386419ea1a7b246a723cf737765cd
JohnPetros <joaopedro.nc@outlook.com>
2024-06-30 21:45:14 -0300

    🚨 errors: add email template not provided error

---
+++ src/app/core/errors/authentication/email_template_not_provided_error.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+from ..base_error import BaseError
+
+
+@dataclass
+class EmailTemplateNotProvidedError(BaseError):
+    ui_message = "Template de e-mail não fornecido"
+    internal_message = "Email template is not provided"
+    status_code = 500
