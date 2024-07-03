7ca83ed9eba3ab55e2acbbf0202c5f24741ee220
JohnPetros <joaopedro.nc@outlook.com>
2024-07-03 11:11:34 -0300

    📑 interfaces: add sensors records repository

---
+++ src/app/core/interfaces/repositories/sensors_record_repository_interface.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from datetime import date
+
+from core.entities import SensorsRecord, LineChartRecord
+
+
+class SensorRecordsRepositoryInterface(ABC):
+    @abstractmethod
+    def create_sensors_record(self, sensors_record: SensorsRecord) -> None: ...
+
+    @abstractmethod
+    def create_many_sensors_records(
+        self, sensors_records: list[SensorsRecord]
+    ) -> None: ...
+
+    @abstractmethod
+    def get_line_charts_sensor_records(self) -> list[dict[str, LineChartRecord]]: ...
+
+    @abstractmethod
+    def get_last_sensors_records(self, count) -> list[SensorsRecord]: ...
+
+    @abstractmethod
+    def get_sensors_record_by_id(self, id: str) -> SensorsRecord | None: ...
+
+    @abstractmethod
+    def get_sensors_records_count(self) -> int: ...
+
+    @abstractmethod
+    def update_sensors_record_by_id(self, sensors_record: SensorsRecord) -> None: ...
+
+    @abstractmethod
+    def delete_many_sensors_records_by_id(self, sensors_record_id: str) -> None: ...
+
+    @abstractmethod
+    def get_filtered_sensors_records(
+        self, plant_id: str, start_date: date, end_date: date, page_number: int = 1
+    ) -> list[SensorsRecord]: ...
