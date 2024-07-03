322553b23097f9d9f371172f38810847eb14c41a
JohnPetros <joaopedro.nc@outlook.com>
2024-07-03 11:57:07 -0300

    🚨 errors: add page number

--- src/app/core/errors/validation/__init__.py
+++ src/app/core/errors/validation/__init__.py
@@ -3,3 +3,4 @@ from .csv_file_not_valid_error import *
 from .datetime_not_valid_error import *
 from .date_not_valid_error import *
 from .sensors_record_not_valid_error import *
+from .page_number_not_valid_error import *
