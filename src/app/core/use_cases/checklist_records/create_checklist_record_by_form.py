3287435270691e494159ef3393c40e29b53e5de1
JohnPetros <joaopedro.nc@outlook.com>
2024-07-04 12:47:16 -0300

    🧪 tests: create checklist record by csv form use case

--- src/app/core/use_cases/checklist_records/create_checklist_record_by_form.py
+++ src/app/core/use_cases/checklist_records/create_checklist_record_by_form.py
@@ -1,48 +1,62 @@
 from datetime import datetime, date
 
 from core.entities.checklist_record import CheckListRecord, Plant
-from core.commons import Error, Date, Datetime
-
-from infra.repositories import checklist_records_repository
+from core.commons import Date, Datetime
+from core.errors.validation import DateNotValidError
+from core.errors.plants import PlantNotFoundError
+from core.interfaces.repositories import (
+    ChecklistRecordsRepositoryInterface,
+    PlantsRepositoryInterface,
+)
 
 
 class CreateChecklistRecordByForm:
+    def __init__(
+        self,
+        checklist_records_repository: ChecklistRecordsRepositoryInterface,
+        plants_repository: PlantsRepositoryInterface,
+    ):
+        self._checklist_records_repository = checklist_records_repository
+        self._plants_repository = plants_repository
+
     def execute(self, request: dict) -> None:
         if not isinstance(request["date"], date):
-            raise Error(ui_message="Data de registro não informado")
+            raise DateNotValidError(ui_message="Data de inserção não válido")
 
-        try:
-            created_at = Datetime(
-                datetime(
-                    hour=request["hour"],
-                    year=request["date"].year,
-                    month=request["date"].month,
-                    day=request["date"].day,
-                )
+        if not isinstance(request["fertilizer_expiration_date"], date):
+            raise DateNotValidError(ui_message="Data de validade de adubo não válido")
+
+        created_at = Datetime(
+            datetime(
+                hour=request["hour"],
+                year=request["date"].year,
+                month=request["date"].month,
+                day=request["date"].day,
             )
+        )
 
-            fertilizer_expiration_date = Date(request["fertilizer_expiration_date"])
+        fertilizer_expiration_date = Date(request["fertilizer_expiration_date"])
 
-            plant = Plant(id=request["plant_id"])
+        plant = self._plants_repository.get_plant_by_id(request["plant_id"])
 
-            checklist_record = CheckListRecord(
-                plantation_type=request["plantation_type"],
-                illuminance=request["illuminance"],
-                lai=request["lai"],
-                soil_humidity=request["soil_humidity"],
-                temperature=request["temperature"],
-                water_consumption=request["water_consumption"],
-                soil_ph=request["soil_ph"],
-                report=request["report"],
-                air_humidity=request["air_humidity"],
-                leaf_color=request["leaf_color"],
-                leaf_appearance=request["leaf_appearance"],
-                fertilizer_expiration_date=fertilizer_expiration_date,
-                created_at=created_at,
-                plant=plant,
-            )
+        if not isinstance(plant, Plant):
+            raise PlantNotFoundError()
 
-            checklist_records_repository.create_checklist_record(checklist_record)
+        checklist_record = CheckListRecord(
+            plantation_type=request["plantation_type"],
+            illuminance=request["illuminance"],
+            lai=request["lai"],
+            soil_humidity=request["soil_humidity"],
+            temperature=request["temperature"],
+            water_consumption=request["water_consumption"],
+            soil_ph=request["soil_ph"],
+            report=request["report"],
+            air_humidity=request["air_humidity"],
+            leaf_color=request["leaf_color"],
+            leaf_appearance=request["leaf_appearance"],
+            fertilizer_expiration_date=fertilizer_expiration_date,
+            created_at=created_at,
+            plant=plant,
+        )
 
-        except Error as error:
-            raise error
+        self._checklist_records_repository.create_checklist_record(checklist_record)
---
+++ src/app/core/use_cases/checklist_records/tests/create_checklist_record_by_form_test.py
@@ -0,0 +1,121 @@
+from datetime import datetime, date
+
+from pytest import fixture, raises
+
+from core.use_cases.tests.mocks.repositories import (
+    ChecklistRecordsRepositoryMock,
+    PlantsRepositoryMock,
+)
+from core.errors.validation import DateNotValidError
+from core.errors.plants import PlantNotFoundError
+from core.entities.tests.fakers import PlantsFaker
+
+
+from ..create_checklist_record_by_form import CreateChecklistRecordByForm
+
+
+def fake_request(base_fake_request: dict):
+    return {
+        "fertilizer_expiration_date": datetime(
+            year=2024, month=3, day=16, hour=12, minute=0
+        ),
+        "date": date(year=2024, month=12, day=12),
+        "hour": 12,
+        "illuminance": 12,
+        "plantation_type": "interno",
+        "lai": 55,
+        "soil_humidity": 20,
+        "air_humidity": 50,
+        "temperature": 24.7,
+        "water_consumption": 0,
+        "soil_ph": 7,
+        "report": "não",
+        "leaf_color": "avermelhada",
+        "leaf_appearance": "um pouco murcha",
+        **base_fake_request,
+    }
+
+
+def describe_create_checklist_record_by_form_use_case():
+    @fixture
+    def checklist_records_repository():
+        return ChecklistRecordsRepositoryMock()
+
+    @fixture
+    def plants_repository():
+        return PlantsRepositoryMock()
+
+    @fixture
+    def use_case(
+        checklist_records_repository: ChecklistRecordsRepositoryMock,
+        plants_repository: PlantsRepositoryMock,
+    ):
+        checklist_records_repository.clear_records()
+        return CreateChecklistRecordByForm(
+            checklist_records_repository=checklist_records_repository,
+            plants_repository=plants_repository,
+        )
+
+    def it_should_throw_error_if_date_from_request_is_not_valid(
+        use_case: CreateChecklistRecordByForm,
+    ):
+        request = fake_request({"date": "not valid date"})
+
+        with raises(DateNotValidError):
+            use_case.execute(request)
+
+    def it_should_throw_error_if_fertilizer_expiration_date_from_request_is_not_valid(
+        use_case: CreateChecklistRecordByForm,
+    ):
+        request = fake_request(
+            {"fertilizer_expiration_date": "not valid fertilizer expiration date"}
+        )
+
+        with raises(DateNotValidError):
+            use_case.execute(request)
+
+    def it_should_throw_error_if_there_is_no_plant_in_repository(
+        use_case: CreateChecklistRecordByForm,
+    ):
+        fake_plant = PlantsFaker.fake()
+
+        request = fake_request({"plant_id": fake_plant.id})
+
+        with raises(PlantNotFoundError):
+            use_case.execute(request)
+
+    def it_should_create_sensors_record(
+        checklist_records_repository: ChecklistRecordsRepositoryMock,
+        plants_repository: PlantsRepositoryMock,
+        use_case: CreateChecklistRecordByForm,
+    ):
+        fake_plant = PlantsFaker.fake()
+        plants_repository.create_plant(fake_plant)
+
+        request = fake_request({"plant_id": fake_plant.id})
+
+        use_case.execute(request)
+
+        last_record = checklist_records_repository.get_last_checklist_records(count=1)[
+            0
+        ]
+
+        assert last_record.soil_humidity == request["soil_humidity"]
+        assert last_record.soil_ph == request["soil_ph"]
+        assert last_record.plantation_type == request["plantation_type"]
+        assert last_record.lai == request["lai"]
+        assert last_record.illuminance == request["illuminance"]
+        assert last_record.leaf_appearance == request["leaf_appearance"]
+        assert last_record.leaf_color == request["leaf_color"]
+        assert last_record.report == request["report"]
+        assert last_record.air_humidity == request["air_humidity"]
+        assert last_record.temperature == request["temperature"]
+        assert last_record.water_consumption == request["water_consumption"]
+        assert (
+            last_record.fertilizer_expiration_date.get_value(is_date=True)
+            == request["fertilizer_expiration_date"]
+        )
+        assert (
+            last_record.created_at.get_value(is_datetime=True).date() == request["date"]
+        )
+        assert last_record.plant == fake_plant
